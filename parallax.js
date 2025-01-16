8962a1b58fa9fcf8e130b7673c3fdfdc0d28d979
Karthik Raj <karthikkap121@gmail.com>
2025-01-16 13:42:47 +0400

    design fix

--- parallax.js
+++ parallax.js
@@ -12,12 +12,33 @@ window.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY;
 
   card1.style.transform = `rotate(${
-    initialRotations.card1 + scrollPosition * 0.01
+    initialRotations.card1 + scrollPosition * 0.04
   }deg)`;
   card2.style.transform = `rotate(${
-    initialRotations.card2 + scrollPosition * -0.01
+    initialRotations.card2 + scrollPosition * -0.04
   }deg)`;
   card3.style.transform = `rotate(${
-    initialRotations.card3 + scrollPosition * 0.01
+    initialRotations.card3 + scrollPosition * 0.04
   }deg)`;
+});
+
+// cup section
+document.addEventListener("scroll", () => {
+  const container = document.querySelector(
+    ".crafter-universe-right--image-container"
+  );
+  const yellowCup = document.querySelector(".crafter-universe-right--image-2");
+
+  const containerRect = container.getBoundingClientRect();
+
+  if (containerRect.top >= 0 && containerRect.bottom <= window.innerHeight) {
+    const scrollProgress =
+      (window.innerHeight - containerRect.top) / window.innerHeight;
+
+    yellowCup.style.transform = `translate(-${scrollProgress * 50}px, -${
+      scrollProgress * 50
+    }px) rotate(-${scrollProgress * 50}deg)`;
+  } else {
+    yellowCup.style.transform = "translateY(0)";
+  }
 });
